40a1452cf596171d019d2a961cb77ddc71a3d8a7
Wilian da Silva Oliveira <wsilva@cleartech.com.br>
2018-08-07 09:23:46 -0300

    first commit

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,58 @@
+var title = document.querySelector(".title");
+
+//alter title
+title.textContent = "Aparecida nutricionista";
+
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdImc = paciente.querySelector(".info-imc");
+
+
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso <= 0 || peso > 1000) {
+        console.log("Peso inválido!");
+        tdPeso.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        
+        paciente.classList.add("paciente-invalido");
+
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida!");
+        tdAltura.textContent = "Altura inválida!";
+        alturaEhValida = false;
+
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+
+        tdImc.textContent = calculaImc(peso, altura);    
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+
+}
+
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+
+
