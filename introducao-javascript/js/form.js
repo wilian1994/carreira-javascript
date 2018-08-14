89a38d8b88c21088d0f3a1fb140c89a014437fb1
Wilian da Silva Oliveira <wsilva@cleartech.com.br>
2018-08-14 09:20:04 -0300

    introducao-javascript finalized

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -5,8 +5,6 @@ botaoAdicionar.addEventListener("click", function(event) {
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     //Adição aqui
     var erros = validaPaciente(paciente);
 
@@ -15,13 +13,16 @@ botaoAdicionar.addEventListener("click", function(event) {
         return;
     }
 
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-    form.reset();
+    form.reset();    
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form){
 
