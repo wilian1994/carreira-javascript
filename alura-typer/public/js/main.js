c84ca114df8384c12ed0218eb6adb9fea09aaeab
Wilian da Silva Oliveira <wsilva@cleartech.com.br>
2018-08-23 09:25:34 -0300

    Game over with events

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -8,3 +8,13 @@ var palavras = frase.split(" ").length;
 console.log(palavras);
 
 var tamFrase = $("#tamanho-frase").text(palavras);
+
+var campo = $(".campo-digitacao");
+
+campo.on("input", function(){
+    var conteudo = campo.val();
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    $("#contador-caracteres").text(conteudo.length);
+})
